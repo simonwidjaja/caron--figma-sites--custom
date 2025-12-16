4c71f4c52b6808ea8e0ff883e166591aa658db0c
Simon Widjaja <widjaja@trendspurt.com>
2025-12-16 15:01:16 +0100

    up

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
